0e0ee3d76dc33c82109fab49505ce918a23bc274
jazon <jason.sevenants@gmail.com>
2022-10-20 16:42:14 +0000

    new20oct16h42

---
+++ test/assets/js/contact.js
@@ -0,0 +1,86 @@
+
+// ____________________________________ EXERCICE 1 JAVASCRIPT____________________________________
+
+// var reponse1 = window.prompt("Entrer votre nom");
+// var reponse2 = window.prompt("Entrer votre prénom");
+
+// if (window.confirm("Etes-vous un Homme ?") == true) 
+// { 
+// }
+// window.alert("Bonjour Monsieur " + reponse1 +" "+ reponse2); 
+
+
+
+// ____________________________________ EXERCICE 2 JAVASCRIPT____________________________________
+
+// var a = 100;
+// var b = 100;
+// var c = 1.00;
+// var d = true;
+
+
+// alert("Chaine de caractères : " + a);
+// alert(b-a);
+// alert(c+a);
+// d = false;
+
+
+// ____________________________________ EXERCICE 3 JAVASCRIPT____________________________________
+
+// const number = prompt("Ecrit un nombre");
+
+// if(number % 2 == 0) {
+//     alert("Le nombre est pair");
+// }
+
+// else {
+//     alert("Le nombre est impair");
+// }
+
+
+
+// -----------------------------------------------------
+
+
+// date = 2022;
+// annee = prompt("Quel est votre année de naissance ? ");
+// reponse1 = date - annee;
+
+// alert("Vous avez " + reponse1 + " ans");
+
+// if(reponse1 <18) {
+// alert("Vous étes mignon, euuu mineur ");
+// }
+
+// else{
+// alert("Vous étes majeur ");
+// }
+
+
+    // -----------------------------------------------     
+
+
+
+
+// er = prompt("Entrer le premier nombre ");
+// eme = prompt("Entrer le deuxième nombre");
+//  if(er, eme < 1) {
+//     alert("division par '0' interdite" );  
+//  }
+
+ 
+// divi = prompt("Entrer un operateur");
+// if(divi != "/") {
+//     alert("erreur de saisi");
+// }
+// result = (er / eme);
+// alert(result);
+
+
+
+// ____________________________________ EXERCICE 4 JAVASCRIPT____________________________________
+
+
+a = ('1');
+b = prompt("Saisissez le prénom N° " + a);
+console.log(a + b);
